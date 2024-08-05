7cf3524739032a7f57c88c11edc544a2ed7d8c5b
Niro-jpg <nicolodentale@gmail.com>
2024-08-05 17:18:14 +0200

    updated main.py

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ from naoqi import ALProxy
 from intro import start_intro
 from Battle import Battle
 
+
+def stt(vocabulary, question, t = 10):
+   global robot
+   query = raw_input()
+   option = 'Invalid'
+   if query!="": # valid answer
+      for i in range(len(vocabulary)):
+          if vocabulary[i] in query:
+              option = i
+              break
+   return option
+
 if __name__=="__main__":
    pdir = os.getenv('PEPPER_TOOLS_HOME')
    sys.path.append(pdir+ '/cmd_server')
@@ -18,11 +30,15 @@ if __name__=="__main__":
    pip = os.getenv('PEPPER_IP')
    pport = 9559
    connection_url = "tcp://" + pip + ":" + str(pport)
-   #pc, oc = start_intro(robot)
+   pc, oc = start_intro(robot)
    while True:
-        
-     #battle = Battle(pepper = True, robot = robot, ally_monster_index = pc, enemy_monster_index = oc)
-     battle = Battle(pepper = True, robot = robot)
+     battle = Battle(pepper = True, robot = robot, ally_monster_index = pc, enemy_monster_index = oc)
      battle.start_battle()
-     break
+     question = 'Hello. What is your name?'
+     question = 'Great battle! Would you like to play again?'
+     robot.say(question)
+     options = ['yes', 'no']
+     option = stt(options, question)
+     if options[option] == 'no':
+        break
    end()
